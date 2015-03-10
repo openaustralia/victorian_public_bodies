3bc2f92687cc0f3df1a2c802355da3142020fc59
Henare Degan <henare.degan@gmail.com>
2015-03-10 15:38:33 +1100

    Skip agencies where we can't work out the ID
    The index page for "X" has a blank link :headdesk:

--- scraper.rb
+++ scraper.rb
@@ -3,13 +3,19 @@ require "scraperwiki"
 require "mechanize"
 
 def save_agency(page)
+  id = page.uri.to_s[/\=(\d+)~$/, 1]
+  if id.nil?
+    puts "Skipping missing ID for URL: #{page.uri.to_s}"
+    return false
+  end
+
   table = page.at('div.DOCbody').at(:table)
   agency_values = table.search('td.AgencyValue').map { |td| td.inner_text }
 
   website = table.search(:a)[1] ? table.search(:a)[1].attr(:href) : nil
 
   record = {
-    id:             page.uri.to_s[/\=(\d+)~$/, 1],
+    id:             id,
     name:           page.at(:h2).inner_text,
     postal_address: agency_values[0].gsub("\n", ", "),
     street_address: agency_values[1].gsub("\n", ", "),
